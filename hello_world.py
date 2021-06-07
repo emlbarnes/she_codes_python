0a18f25ca8da4a22a3668ab220028b6596e886e7
Emily Barnes <33987921+emlbarnes@users.noreply.github.com>
2021-06-07 19:52:21 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+print("Hello World")
+
+day="Monday"
+print(day)
+print (f"Today is:{day}")
+
+month="June"
+day = "Monday"
+print(month)
+print(f"Today is: {month} and the day is {day}")
+
+
+run1_dist = 1400
+print(run1_dist)
+print(type(run1_dist))
+run2_dist = 1800
+
+total_distance = run1_dist + run2_dist
+difference= run2_dist - run1_dist
+print(total_distance)
+print(difference)
+
+run3_dist = 1.7
+run4_dist = 1.35
+print(type(run4_dist))
+
+print(run1_dist / 1000)
+print(run4_dist * 1000)
