f6cdf93874cac735dc173ddac9a044b711198d3b
Emily Barnes <33987921+emlbarnes@users.noreply.github.com>
2021-06-08 10:38:09 +0800

    practice python

---
+++ user_input_playground/user_input_playground.py
@@ -0,0 +1,12 @@
+name = input ("What is your name? ")
+print (f"Hi {name}!")
+
+age = input(f"Hi{name}, how old are you?")
+years_unit_100 = 100 - int(age)
+print(f"Wow {name}! You'll be 100 in {years_until_100}years!")
+
+number1 = input("enter a number")
+number2 = input("enter another number")
+total = int(number1)+int(number2)
+print (f "these two numbers")= {total})
+
